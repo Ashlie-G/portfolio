485cb9060e52336a4a6c07cc8d8ae71ca5cde79e
Ashlie Gray <ashliejanegray@gmail.com>
2020-11-23 15:10:46 +1000

    hopefully changed to postgres now

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_11_20_123401) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "skills", force: :cascade do |t|
     t.string "name"
     t.text "description"
